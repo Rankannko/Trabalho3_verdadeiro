d6247217291e8feb872d783b52b7ddf9573c0c7e
Rankannko <azevedomasterjp27@hotmail.com>
2020-09-25 11:33:46 -0300

    Tentativas de criar menu
    Por algum motivo não esta funcionando, deixar este trabalho para depois

--- src/br/maua/Main.java
+++ src/br/maua/Main.java
@@ -1,8 +1,10 @@
 package br.maua;
+import br.maua.classes.Menu;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.run();
     }
 }
--- src/br/maua/classes/Menu.java
+++ src/br/maua/classes/Menu.java
@@ -1,4 +1,35 @@
 package br.maua.classes;
 
+import java.util.Scanner;
+
 public class Menu {
+
+    private Scanner scanner;
+
+    public void run(){
+        boolean alive = true;
+
+        do{
+            this.menuvisual();
+            int opcao = Integer.parseInt(this.scanner.next());
+            switch(opcao){
+            case 0:
+                alive = false;
+                break;
+                case 1:
+                System.out.println("Você escolheu animes:");
+            case 2:
+                System.out.println("Voce escolheu mangas:");
+            default:
+                System.out.println("Não é um numero valido, escolha uma opção que está no menu");
+            }
+        }while (alive);
+    }
+
+    private static void menuvisual(){
+        System.out.println("Escolha o que você quer checar:");
+        System.out.println("1-Anime");
+        System.out.println("2-Manga");
+        System.out.println("0-Sair");
+    }
 }
