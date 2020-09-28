894fd0aee680fddd549e4111615855574fe6815a
Rankannko <azevedomasterjp27@hotmail.com>
2020-09-28 19:23:15 -0300

    Gerados getters e setters das classes Anime e Manga

--- src/br/maua/classes/Anime.java
+++ src/br/maua/classes/Anime.java
@@ -1,5 +1,35 @@
 package br.maua.classes;
 
 public class Anime {
-    
+    public Anime(String URL, String nome, String sinopse, int episodios, float nota) {
+        this.URL = URL;
+        Nome = nome;
+        Sinopse = sinopse;
+        Episodios = episodios;
+        Nota = nota;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public String getSinopse() {
+        return Sinopse;
+    }
+
+    public int getEpisodios() {
+        return Episodios;
+    }
+
+    public float getNota() {
+        return Nota;
+    }
+
+    private String URL, Nome, Sinopse;
+    private int Episodios;
+    private float Nota;
 }
--- src/br/maua/classes/Manga.java
+++ src/br/maua/classes/Manga.java
@@ -1,4 +1,40 @@
 package br.maua.classes;
 
 public class Manga {
+    public Manga(String URL, String nome, String tipo, int capitulos, int volumes, float nota) {
+        this.URL = URL;
+        Nome = nome;
+        Tipo = tipo;
+        Capitulos = capitulos;
+        Volumes = volumes;
+        Nota = nota;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public int getCapitulos() {
+        return Capitulos;
+    }
+
+    public int getVolumes() {
+        return Volumes;
+    }
+
+    public float getNota() {
+        return Nota;
+    }
+
+    private String URL, Nome, Tipo;
+    private int Capitulos, Volumes;
+    private float Nota;
 }
